2c898b7ea4b85f81525de2362e1736b7716c2662
Troels Knak-Nielsen <troelskn@gmail.com>
2011-07-29 17:50:05 +0200

    Annotated config options

--- config.php
+++ config.php
@@ -1,8 +1,15 @@
 <?php
+// Database credentials
 $db_dsn = "mysql:host=localhost;dbname=fejlvarp";
 $db_username = "root";
 $db_password = null;
 
-$server_name = "http://errors.localhost/";
-$mail_recipient = "error@example.com";
+// Enter the uri of the fejlvarp application here
+$server_name = "http://localhost/fejlvarp";
 
+// Specify mail address to mail when a new incident occurs
+$mail_recipient = null;
+
+// Specify notifo details to notiy when a new incident occurs
+$notifo_username = null;
+$notifo_apisecret = null;
